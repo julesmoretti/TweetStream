add28ac73effb978d015c457b65b5f3c64fa9e99
Nick Loveridge <nick@fusemedia.ca>
2013-12-16 12:12:00 -0800

    Moved index.html outside of /public dir. Simple fix for socket.io-client side library requirement. Also added socket.io client side npm module"

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var config = require('./config');
 // Connect to twitter API
 var twit = new twitter(config.twitter);
 
+// TODO: Check for valid oAuth tokens before re-authenticating.
 // twit.verifyCredentials(function (err, data) {
   // console.log('Twitter auth response', err);
 // });
-
 
 
 // Start up webserver.
@@ -19,7 +19,7 @@ var port = 3000;
 
 var app = express();
 app.get('/', function(req, res){
-  res.sendfile(__dirname + '/public/index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 
 app.listen(port);
