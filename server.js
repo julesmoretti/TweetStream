9bff69083297ff9432a26ed8243f7ed3e577fd24
Nick Loveridge <nick@fusemedia.ca>
2013-12-16 11:56:49 -0800

    replaced nTwitter with node-twitter. nTwitter is currently not compatible with Twitter API v1.1 where as node-twitter appears to be. Commented out Twitter auth due to 15 request per 15 minutes rate limiting

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var socketio = require('socket.io');
-var twitter = require('ntwitter');
+// var twitter = require('ntwitter');
+var twitter = require('twitter');
 var config = require('./config');
+
+
+// Connect to twitter API
+var twit = new twitter(config.twitter);
+
+// twit.verifyCredentials(function (err, data) {
+  // console.log('Twitter auth response', err);
+// });
+
+
 
 // Start up webserver.
 var port = 3000;
