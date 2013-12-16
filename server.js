135292ca494647ed196a2b6e732ce8391712130b
Nick Loveridge <nick@fusemedia.ca>
2013-12-16 11:25:44 -0800

    Added ntwitter for Twitter API auth. Config file added for API keys, but added to ignore list so they remain unpublished

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var socketio = require('socket.io');
+var twitter = require('ntwitter');
+var config = require('./config');
 
 // Start up webserver.
 var port = 3000;
 
 var app = express();
 app.get('/', function(req, res){
-  // res.send('hello world');
   res.sendfile(__dirname + '/public/index.html');
 });
 
