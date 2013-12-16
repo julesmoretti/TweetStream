4f07384987c658cf7abeba28ad8098895a895406
Nick Loveridge <nick@fusemedia.ca>
2013-12-16 11:19:45 -0800

    Express server running and serving static html

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 var express = require('express');
-var socketio = require('express');
+var socketio = require('socket.io');
 
-console.log('Hello World');
+// Start up webserver.
+var port = 3000;
+
+var app = express();
+app.get('/', function(req, res){
+  // res.send('hello world');
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+app.listen(port);
+console.log('Express is running. Open http://localhost:' + port);
